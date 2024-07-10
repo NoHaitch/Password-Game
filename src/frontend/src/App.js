0132a5a81b12971e17101be42b4a0d8e1d85ab8c
Francisco <franciscotrianto@gmail.com>
2024-07-10 17:14:26 +0700

    feat: add title and password field

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PasswordField from "./components/PasswordField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-screen h-screen justify-center">
+      <div className="flex flex-col h-screen w-[90%] items-center p-6">
+        <div className="text-center">
+          <h1 className="text-5xl text-white font-title m-4">The Password Game</h1>
+          <h2 className="text-xl text-white font-mono">inspired by the <a href="https://neal.fun/password-game/" className="underline">original</a></h2>
+        </div>
+        <div className="content m-4">
+          <PasswordField />
+        </div>
+      </div>
     </div>
   );
 }
